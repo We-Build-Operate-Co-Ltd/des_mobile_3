58e220779e065f44135259e7f864faf8a5730c8f
panuwit <>
2023-03-21 16:31:10 +0700

    fixed

--- lib/report_problem.dart
+++ lib/report_problem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -10,6 +11,8 @@ class ReportProblem extends StatefulWidget {
 }
 
 class _ReportProblemState extends State<ReportProblem> {
+  TextEditingController chatController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -22,14 +25,14 @@ class _ReportProblemState extends State<ReportProblem> {
       },
       child: Scaffold(
         backgroundColor: Color(0xFFFFFFFF),
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          automaticallyImplyLeading: false,
-          toolbarHeight: 10,
-        ),
+        // appBar: AppBar(
+        //   elevation: 0,
+        //   backgroundColor: Colors.white,
+        //   automaticallyImplyLeading: false,
+        //   toolbarHeight: 10,
+        // ),
         body: SafeArea(
-          top: false,
+          // top: false,
           child: GestureDetector(
               onTap: () {
                 FocusScope.of(context).requestFocus(new FocusNode());
@@ -48,6 +51,7 @@ class _ReportProblemState extends State<ReportProblem> {
         children: [
           Container(
             margin: EdgeInsets.only(bottom: 10),
+            padding: EdgeInsets.only(top: 10),
             decoration: BoxDecoration(
               color: Colors.white,
               // borderRadius: BorderRadius.circular(20),
@@ -117,8 +121,7 @@ class _ReportProblemState extends State<ReportProblem> {
                         Icons.close,
                         size: 35,
                         color: Color(0xFFF7A4CB1),
-                      )
-                      ),
+                      )),
                 ],
               ),
             ),
@@ -132,22 +135,256 @@ class _ReportProblemState extends State<ReportProblem> {
 
   _body() {
     return SafeArea(
-      child: Container(
-        height: MediaQuery.of(context).size.longestSide,
-        width: MediaQuery.of(context).size.longestSide,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-              'assets/report_mock.png',
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          flex: 10,
+          child: Container(
+            height: 20,
+            width: MediaQuery.of(context).size.width,
+            color: Color(0xFFFEF7FF),
+            child: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF7F34EE),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Image.asset(
+                            'assets/chat.png',
+                            height: 26,
+                            width: 26,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        // height: 70,
+                        // width: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(20),
+                            bottomLeft: Radius.circular(20),
+                            bottomRight: Radius.circular(10),
+                          ),
+                          color: Color(0xFF7F34EE).withOpacity(0.15),
+                          // shape: BoxShape.circle,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text(
+                            'สวัสดีค่ะ ท่านต้องการความช่วยเหลือ\nด้านไหนสามารถเลือกหัวข้อได้ หรือ\nจะส่งเป็นข้อความก็ได้ค่ะ',
+                            style: TextStyle(
+                              fontSize: 11,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      child: Container(
+                        // height: 70,
+                        // width: 70,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(30),
+                            ),
+                            border: Border.all(
+                                color: Color(0xFF7F34EE),
+                                width: 1,
+                                style: BorderStyle.solid),
+                            color: Colors.white
+                            // shape: BoxShape.circle,
+                            ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text(
+                            'การจองใช้บริการ',
+                            style: TextStyle(
+                              fontSize: 11,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      child: Container(
+                        // height: 70,
+                        // width: 70,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(30),
+                            ),
+                            border: Border.all(
+                                color: Color(0xFF7F34EE),
+                                width: 1,
+                                style: BorderStyle.solid),
+                            color: Colors.white
+                            // shape: BoxShape.circle,
+                            ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text(
+                            'การยืนยันตัวตน',
+                            style: TextStyle(
+                              fontSize: 11,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      child: Container(
+                        // height: 70,
+                        // width: 70,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(30),
+                            ),
+                            border: Border.all(
+                                color: Color(0xFF7F34EE),
+                                width: 1,
+                                style: BorderStyle.solid),
+                            color: Colors.white
+                            // shape: BoxShape.circle,
+                            ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text(
+                            'สมัครสมาชิก',
+                            style: TextStyle(
+                              fontSize: 11,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              
+              ],
             ),
-            fit: BoxFit.fill,
           ),
         ),
-        // child: SizedBox(),
+        Flexible(
+          flex: 2,
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                // height: 20,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.white,
+                child: 
+                
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                   Expanded(
+                    flex: 10,
+                     child: TextFormField(
+                               // obscureText: showTxtPasswordOld,
+                               style: TextStyle(
+                                 color: Colors.black,
+                                 fontWeight: FontWeight.normal,
+                                 fontFamily: 'Kanit',
+                                 fontSize: 15.0,
+                               ),
+                               decoration: InputDecoration(
+                                 
+                                 filled: true,
+                                 fillColor: Colors.white,
+                                 contentPadding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+                                 hintText: 'รหัสผ่านปัจจุบัน',
+                                 border: OutlineInputBorder(
+                                   borderRadius: BorderRadius.circular(10.0),
+                                   borderSide: BorderSide.none,
+                                 ),
+                                 errorStyle: TextStyle(
+                                   fontWeight: FontWeight.normal,
+                                   fontFamily: 'Kanit',
+                                   fontSize: 10.0,
+                                 ),
+                               ),
+                               // validator: (model) {
+                               //   if (model.isEmpty) {
+                               //     return 'กรุณากรอกรหัสผ่านปัจจุบัน.';
+                               //   }
+                               // },
+                               controller: chatController,
+                               enabled: true,
+                             ),
+                   ),
+          
+                    // Text('ความต้องการของท่าน'),
+                    Flexible(
+                      flex: 1,
+                      child: Image.asset(
+                        'assets/send.png',
+                        height: 26,
+                        width: 26,
+                      ),
+                    ),
+                  ],
+                )
+                ))
+      ],
+    )
+
+        // Container(
+        //   height: MediaQuery.of(context).size.longestSide,
+        //   width: MediaQuery.of(context).size.longestSide,
+        //   decoration: BoxDecoration(
+        //     image: DecorationImage(
+        //       image: AssetImage(
+        //         'assets/report_mock.png',
+        //       ),
+        //       fit: BoxFit.fill,
+        //     ),
+        //   ),
+        //   // child: SizedBox(),
+        //   // ),
+        //   // Expanded(child: SizedBox())
+        //   // ],
         // ),
-        // Expanded(child: SizedBox())
-        // ],
-      ),
-    );
+
+        );
   }
 }
