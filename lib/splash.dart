eb195bb7484c37d74f78b432f0d8909733b91321
jiravong <webuild.developer@gmail.com>
2024-03-13 11:14:05 +0700

    fix splash

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,132 +1,86 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:des/login_first.dart';
 import 'package:des/shared/extension.dart';
 import 'package:des/shared/secure_storage.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:des/menu.dart';
+import 'package:intl/intl.dart';
 import 'check_version.dart';
 import 'shared/config.dart';
 
 class SplashPage extends StatefulWidget {
-  const SplashPage({super.key});
+  const SplashPage({
+    Key? key,
+    this.code,
+  }) : super(key: key);
+  final String? code;
 
   @override
   State<SplashPage> createState() => _SplashPageState();
 }
 
 class _SplashPageState extends State<SplashPage> {
-  Future<dynamic>? futureModel;
-  String _profileCode = '';
-  List<dynamic> _model = [];
+  dynamic profile;
+  String now = DateFormat('dd/MM/yyyy').format(DateTime.now());
   late int version_store;
   dynamic _model_version;
   String os = Platform.operatingSystem;
-  bool _loading = true;
-  String _image = '';
+  String _urlImage = '';
+  int _timeOut = 1000;
+  Future<dynamic>? futureModel;
 
   @override
   void initState() {
-    _callRead();
+    version_store = versionNumber;
+    _getImage();
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () {
-        return Future.value(false);
-      },
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: _buildSplash(),
-      ),
-    );
+  void dispose() {
+    super.dispose();
   }
 
-  _buildSplash() {
-    if (!_loading) {
-      if (_image != '') {
-        return Center(
-          child: CachedNetworkImage(
-            imageUrl: _image,
-            fit: BoxFit.cover,
-            height: double.infinity,
-            width: double.infinity,
-          ),
-        );
-      }
-    }
-    return Container(
-      color: Colors.white,
-    );
-  }
-
-  _callRead() async {
+  void _getImage() async {
     try {
-      var pf = await ManageStorage.read('profileCode') ?? '';
-
-      Dio dio = Dio();
       Response result =
-          await dio.post('$server/dcc-api/m/splash/read', data: {});
+          await Dio().post('$server/dcc-api/m/splash/read', data: {});
+      logE('load splash screen');
 
-      if (result.statusCode == 200) {
-        if (result.data['status'] == 'S') {
-          setState(() {
-            _profileCode = pf;
-            _model = result.data['objectData'];
-            _image = _model.length > 0 ? _model[0]['imageUrl'] : '';
-          });
-        }
+      if (result.data['objectData'].length > 0) {
+        setState(() {
+          _urlImage = result.data['objectData'][0]['imageUrl'];
+          _timeOut = int.parse(result.data['objectData'][0]['timeOut'] ?? 0);
+        });
       }
-      setState(() => _loading = false);
-      _callTimer();
+      logWTF(_urlImage);
+      int time = (_timeOut / 1000).round();
+      Timer(Duration(seconds: time), _checkVersion);
     } catch (e) {
-      setState(() => _loading = false);
+      logE('catch splash screen');
+      _checkVersion();
+    }
+  }
+
+  void _checkVersion() async {
+    try {
+      String os_device = os == 'ios' ? 'Ios' : 'Android-DCC';
+      // print('os : ${os_device}');
+      Dio dio = Dio();
+      var res = await dio
+          .post('$server/dcc-api/version/read', data: {"platform": os_device});
+      setState(() {
+        version_store =
+            int.parse(res.data['objectData'][0]['version'].split('.').join(''));
+        _model_version = res.data['objectData'][0];
+      });
+      navigationPage();
+    } catch (e) {
       logE(e);
-      _callTimer();
+      navigationPage();
     }
-  }
-
-  _callTimer() async {
-    int time = 0;
-    if (_model.length > 0) {
-      time = int.parse((_model[0]?['timeOut'] ?? '0'));
-    }
-    var calTime = time > 0 ? (time / 1000).round() : 0;
-
-    Timer(Duration(seconds: calTime), () {
-      check_version();
-
-      // if (_profileCode != '') {
-      //   check_version();
-      // } else {
-      //   if (!mounted) return;
-      //   Navigator.of(context).pushAndRemoveUntil(
-      //     MaterialPageRoute(
-      //       builder: (context) => const LoginFirstPage(),
-      //     ),
-      //     (Route<dynamic> route) => false,
-      //   );
-      // }
-    });
-  }
-
-  void check_version() async {
-    String os_device = os == 'ios' ? 'Ios' : 'Android-DCC';
-    // print('os : ${os_device}');
-    Dio dio = Dio();
-    var res = await dio
-        .post('$server/dcc-api/version/read', data: {"platform": os_device});
-    setState(() {
-      version_store =
-          int.parse(res.data['objectData'][0]['version'].split('.').join(''));
-      _model_version = res.data['objectData'][0];
-    });
-
-    navigationPage();
   }
 
   Future<void> navigationPage() async {
@@ -135,12 +89,13 @@ class _SplashPageState extends State<SplashPage> {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-          // builder: (context) => const IntroductionPage(),
           builder: (context) => CheckVersionPage(model: _model_version),
         ),
       );
     } else {
-      if (_profileCode != '') {
+      String accessToken = await ManageStorage.read('accessToken');
+      if (!mounted) return;
+      if (accessToken.isNotEmpty) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
@@ -148,14 +103,50 @@ class _SplashPageState extends State<SplashPage> {
           ),
         );
       } else {
-        if (!mounted) return;
-        Navigator.of(context).pushAndRemoveUntil(
+        Navigator.pushReplacement(
+          context,
           MaterialPageRoute(
             builder: (context) => const LoginFirstPage(),
           ),
-          (Route<dynamic> route) => false,
         );
       }
     }
+  }
+
+  _check() {
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(
+        builder: (context) => const Menu(),
+      ),
+      (Route<dynamic> route) => false,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      extendBody: true,
+      body: Container(
+        alignment: Alignment.bottomCenter,
+        child: Image.network(
+          _urlImage,
+          width: double.infinity,
+          height: double.infinity,
+          fit: BoxFit.fill,
+          errorBuilder: (context, error, stackTrace) {
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  height: 100,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
